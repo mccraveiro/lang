40b584972db54c5564e1bf3438753a948bd39d2e
Mateus Craveiro <mateus.craveiro@pagar.me>
2017-08-06 19:27:14 -0300

    compiler: add prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+const prettier = require('prettier')
 const lexer = require('./lexer')
 const Program = require('./ast/program')
 
 const compile = (sourcecode) => {
   const tokens = lexer(sourcecode)
   const AST = new Program(tokens)
+  const code = AST.generate()
 
-  return AST.generate()
+  return prettier.format(code, {
+    semi: false,
+  })
 }
 
 module.exports = {
-  compile
+  compile,
 }
