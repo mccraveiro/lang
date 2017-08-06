e54fa9da92f94560796dbdbb065e4a7ca133cee6
Mateus Craveiro <mateus.craveiro@pagar.me>
2017-08-05 23:20:09 -0300

    runtime: add evaluation step

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const filepath = process.argv[2]
 const sourcecode = readFileSync(filepath, 'utf-8')
 
 const output = parse(sourcecode)
+const result = eval(output)
 
-console.log(output);
+console.log(result)
