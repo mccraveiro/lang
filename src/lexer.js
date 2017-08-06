cd9767be30f4af44233981c948f84b1c539d483d
Mateus Craveiro <mateus.craveiro@pagar.me>
2017-08-06 13:42:55 -0300

    lexer: improve token identification

--- src/lexer.js
+++ src/lexer.js
@@ -1,15 +1,37 @@
-module.exports = sourcecode =>
-  sourcecode.split(/\s+/)
-    .filter(token => token.length > 0)
-    .map((token) => {
-      if (token === '|>') {
-        return { type: 'pipe' }
-      }
+const identifyToken = (token) => {
+  const NAME = /[a-zA-Z]+/
+  const NUMBER = /[0-9]+/
 
-      if (isNaN(token)) {
-        return { type: 'name', value: token }
-      } else {
-        return { type: 'number', value: token }
-      }
-    })
+  if (token === '|>') {
+    return {
+      type: 'pipe'
+    }
+  }
+
+  if (token.match(NAME)) {
+    return {
+      type: 'name',
+      value: token
+    }
+  }
+
+  if (token.match(NUMBER)) {
+    return {
+      type: 'number',
+      value: token
+    }
+  }
+
+  throw new Error(`Unkown token ${token}`)
+}
+
+const lineLexer = sourcecode =>
+  sourcecode.split(' ')
+    .filter(token => token.length > 0)
+    .map(identifyToken)
+
+module.exports = sourcecode =>
+  sourcecode.split('\n')
+    .map(lineLexer)
+    .reduce((tokens, line) => tokens.concat(line), [])
 
