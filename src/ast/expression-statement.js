05ea57e24c4ec1b7a4a5907f8c25dcc56cb0a4e5
Mateus Craveiro <mateus.craveiro@pagar.me>
2017-08-06 02:56:55 -0300

    ast: add suport for nested expression-statements

--- src/ast/expression-statement.js
+++ src/ast/expression-statement.js
@@ -26,6 +26,14 @@ class ExpressionStatement {
               type: 'NumberLiteral',
               value: argumentB.value
             })
+          } else if (argumentA.type === 'number' && argumentB.type === 'function') {
+            this.arguments.push({
+              type: 'NumberLiteral',
+              value: argumentA.value
+            })
+
+            tokens.unshift(argumentB)
+            this.arguments.push(new ExpressionStatement(tokens))
           } else {
             throw 'add should receive two numbers as arguments'
           }
@@ -48,6 +56,14 @@ class ExpressionStatement {
               type: 'NumberLiteral',
               value: argumentB.value
             })
+          } else if (argumentA.type === 'number' && argumentB.type === 'function') {
+            this.arguments.push({
+              type: 'NumberLiteral',
+              value: argumentA.value
+            })
+
+            tokens.unshift(argumentB)
+            this.arguments.push(new ExpressionStatement(tokens))
           } else {
             throw 'subtract should receive two numbers as arguments'
           }
@@ -63,7 +79,16 @@ class ExpressionStatement {
       subtract: '-',
     }
 
-    return `${this.arguments[0].value} ${symbols[this.name]} ${this.arguments[1].value}`
+    const output = `${this.arguments[0].value} ${symbols[this.name]}`
+
+    if (
+      this.arguments[0].type === 'NumberLiteral' &&
+      this.arguments[1].type === 'NumberLiteral'
+    ) {
+      return `${output} ${this.arguments[1].value}`
+    } else {
+      return `${output} ${this.arguments[1].generate()}`
+    }
   }
 }
 
