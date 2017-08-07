24b21e21eda35b4ede85e4c34f1de664c4f1ff9f
Mateus Craveiro <mateus.craveiro@pagar.me>
2017-08-06 21:16:57 -0300

    repl: add basic evaluation

---
+++ repl.js
@@ -0,0 +1,16 @@
+#! /usr/bin/node
+
+/* eslint-disable no-eval */
+
+const repl = require('repl')
+const { compile } = require('./src')
+
+const evaluate = (cmd, context, filename, callback) => {
+  const code = compile(cmd)
+  const result = eval(code)
+  callback(null, result)
+}
+
+repl.start({
+  eval: evaluate,
+})
