7072892f40b2c8d8ceca7bc747ced8de91fa1342
Mateus Craveiro <mccraveiro@gmail.com>
2017-08-20 00:17:15 -0300

    repl: combine with executable
    closes #8

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,27 @@
 #! /usr/bin/env node
 /* eslint-disable no-eval, no-console */
 
+const repl = require('repl')
 const { readFileSync } = require('fs')
 const { compile } = require('../src')
 
 const filepath = process.argv[2]
-const sourcecode = readFileSync(filepath, 'utf-8')
 
-const output = compile(sourcecode)
-const result = eval(output)
+if (filepath) {
+  const sourcecode = readFileSync(filepath, 'utf-8')
+  const output = compile(sourcecode)
+  const result = eval(output)
 
-console.log(result)
+  console.log(result)
+  process.exit(0)
+}
+
+const evaluate = (cmd, context, filename, callback) => {
+  const code = compile(cmd)
+  const result = eval(code)
+  callback(null, result)
+}
+
+repl.start({
+  eval: evaluate,
+})
